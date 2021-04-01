b60413e968165eec98eebed635d419267beee154
rafaelpontes13@hotmail.com <Rafa91197353>
2021-03-31 22:21:33 -0300

    Estrutura algoritmo repetição espaçada

---
+++ src/test/java/com/flashcard/service/Sm2ServiceTest.java
@@ -0,0 +1,17 @@
+package com.flashcard.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Sm2ServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void execute() {
+    }
+}
